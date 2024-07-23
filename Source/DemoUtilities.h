c5758a59b451fec0780260961088ea7e6c3d9891
Vincent Berthiaume <vberthiaume@gmail.com>
2024-07-23 10:28:04 -0400

    cleanup

--- Source/DemoUtilities.h
+++ Source/DemoUtilities.h
@@ -49,42 +49,7 @@ inline Colour getUIColourIfAvailable (LookAndFeel_V4::ColourScheme::UIColour uiC
     return fallback;
 }
 
-inline File getExamplesDirectory() noexcept
-{
-   #ifdef PIP_JUCE_EXAMPLES_DIRECTORY
-    MemoryOutputStream mo;
 
-    auto success = Base64::convertFromBase64 (mo, JUCE_STRINGIFY (PIP_JUCE_EXAMPLES_DIRECTORY));
-    ignoreUnused (success);
-    jassert (success);
-
-    return mo.toString();
-   #elif defined PIP_JUCE_EXAMPLES_DIRECTORY_STRING
-    return File { CharPointer_UTF8 { PIP_JUCE_EXAMPLES_DIRECTORY_STRING } };
-   #else
-    auto currentFile = File::getSpecialLocation (File::SpecialLocationType::currentApplicationFile);
-    auto exampleDir = currentFile.getParentDirectory().getChildFile ("examples");
-
-    if (exampleDir.exists())
-        return exampleDir;
-
-    // keep track of the number of parent directories so we don't go on endlessly
-    for (int numTries = 0; numTries < 15; ++numTries)
-    {
-        if (currentFile.getFileName() == "examples")
-            return currentFile;
-
-        const auto sibling = currentFile.getSiblingFile ("examples");
-
-        if (sibling.exists())
-            return sibling;
-
-        currentFile = currentFile.getParentDirectory();
-    }
-
-    return currentFile;
-   #endif
-}
 
 enum class AssertAssetExists
 {
@@ -92,45 +57,7 @@ enum class AssertAssetExists
     yes
 };
 
-inline std::unique_ptr<InputStream> createAssetInputStream (const char* resourcePath,
-                                                            [[maybe_unused]] AssertAssetExists assertExists = AssertAssetExists::yes)
-{
-  #if JUCE_ANDROID
-    ZipFile apkZip (File::getSpecialLocation (File::invokedExecutableFile));
-    const auto fileIndex = apkZip.getIndexOfFileName ("assets/" + String (resourcePath));
 
-    if (fileIndex == -1)
-    {
-        jassert (assertExists == AssertAssetExists::no);
-        return {};
-    }
-
-    return std::unique_ptr<InputStream> (apkZip.createStreamForEntry (fileIndex));
-  #else
-   #if JUCE_IOS
-    auto assetsDir = File::getSpecialLocation (File::currentExecutableFile)
-                          .getParentDirectory().getChildFile ("Assets");
-   #elif JUCE_MAC
-    auto assetsDir = File::getSpecialLocation (File::currentExecutableFile)
-                          .getParentDirectory().getParentDirectory().getChildFile ("Resources").getChildFile ("Assets");
-
-    if (! assetsDir.exists())
-        assetsDir = getExamplesDirectory().getChildFile ("Assets");
-   #else
-    auto assetsDir = getExamplesDirectory().getChildFile ("Assets");
-   #endif
-
-    auto resourceFile = assetsDir.getChildFile (resourcePath);
-
-    if (! resourceFile.existsAsFile())
-    {
-        jassert (assertExists == AssertAssetExists::no);
-        return {};
-    }
-
-    return resourceFile.createInputStream();
-  #endif
-}
 
 inline Image getImageFromAssets (const char* assetName)
 {
